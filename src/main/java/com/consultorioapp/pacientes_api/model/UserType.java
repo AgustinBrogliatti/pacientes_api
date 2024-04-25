3d940f0d13d42544a368e7cd60aad238750b4be9
AgustinBrogliatti <agustinbrogliatti@gmail.com>
2024-04-25 12:23:23 -0300

    Add userType enum

---
+++ src/main/java/com/consultorioapp/pacientes_api/model/UserType.java
@@ -0,0 +1,8 @@
+package com.consultorioapp.pacientes_api.model;
+
+
+public final class UserType {
+    public static final String DOCTOR = "DR";
+    public static final String SECRETARY = "ADMIN";
+}
+
